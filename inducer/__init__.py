2d85a07500f56eac9d988ec05d9c3b1360160240
fras2560 <dallas_j_frase@hotmail.com>
2014-09-11 10:36:24 -0400

    Put in basic documentation

---
+++ inducer/__init__.py
@@ -0,0 +1,12 @@
+"""
+-------------------------------------------------------
+inducer
+a python class for helping determine if G contains
+an induced subgraph H
+-------------------------------------------------------
+Author:  Dallas Fraser
+ID:      110242560
+Email:   fras2560@mylaurier.ca
+Version: 2014-09-11
+-------------------------------------------------------
+"""
