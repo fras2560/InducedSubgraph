8d1b960b2a5debbeb74137b350a349035f92a76a
fras2560 <dallas_j_frase@hotmail.com>
2014-10-06 17:03:33 -0400

    A brute force algo to determine the chromatic number of a graph

--- inducer/colorable.py
+++ inducer/colorable.py
@@ -38,19 +38,6 @@ def valid_coloring(coloring, G):
             break;
     return valid
 
-def color_permutations(nodes, baskets):
-    '''
-    a function that returns a iterator for all the possible ways to split the
-    nodes into number of baskets
-    Parameters:
-        nodes: the list of nodes (order does matter)
-        baskets: the number of baskets available (int)
-    Returns:
-        iterator: of all the combinations
-    '''
-    # TODO
-    pass
-
 def chromatic_number(G):
     '''
     a function that finds the chromatic number of graph G
@@ -60,19 +47,135 @@ def chromatic_number(G):
     Returns:
         chromatic: the chromatic number (int)
     '''
-    chromatic = 0
-    valid = False
+    chromatic = 1
+    valid = True
     nodes = G.nodes()
+    balls = len(nodes)
+    # manually check the one case 
+    for node in G.nodes():
+        if len(G.neighbors(node)) != 0:
+            valid = False
+            break;
     while not valid:
         chromatic += 1
+        print('''
+                ------------------------------
+                Testing Chromatic Number of %s
+                ------------------------------
+              ''' %chromatic)
+        boxes = [balls] * chromatic
         for combo in permutations(nodes):
-            for coloring in color_permutations(combo, chromatic):
-                if valid_coloring(coloring, G):
-                    valid = True
-                    break;
+            for split in unlabeled_balls_in_unlabeled_boxes(balls, boxes):
+                coloring = None
+                if valid_split(split):
+                    node_combo = convert_combo(combo)
+                    coloring = assemble_coloring(node_combo, split)
+                if coloring is not None:
+                    if valid_coloring(coloring, G):
+                        valid = True
+                        break;
             if valid:
                 break
+        if chromatic > 10:
+            # stop case
+            valid = True
+            chromatic = None
+        if chromatic == len(nodes):
+            valid = True
     return chromatic
+
+def valid_split(split):
+    '''
+    a function that checks if the split of nodes is valid 
+    for that number of colors
+    Parameters:
+        split: the split of each color (tuple)
+    Returns:
+        True if valid
+        False otherwise
+    '''
+    valid = True
+    for color in split:
+        if color == 0:
+            valid = False
+    return valid
+
+def convert_combo(combo):
+    '''
+    a function that converts a combo tuple to a list
+    Parameters:
+        combo: a tuple of combinations (tuple)
+    Returns:
+        conversion: the converted combination (list)
+    '''
+    conversion = []
+    for c  in combo:
+        conversion.append(c)
+    return conversion
+
+def assemble_coloring(nodes, split):
+    '''
+    a function that assembles the coloring
+    Parameters:
+        nodes: the list of node (list)
+        split: the list of split (how many nodes for each color) (list)
+    Returns:
+        coloring: list of color's list
+                 E.G.  [[n1, n2], [n3]]
+    '''
+    coloring = None
+    if len(split) == 0:
+        coloring = [nodes]
+    else:
+        coloring = []
+        for size in split:
+            color = []
+            while len(color) < size:
+                color.append(nodes.pop())
+            coloring.append(color)
+    return coloring
+    
+def unlabeled_balls_in_unlabeled_boxes(balls, box_sizes):
+    '''
+    @author Dr. Phillip M. Feldman
+    '''
+    if not isinstance(balls, int):
+        raise TypeError("balls must be a non-negative integer.")
+    if balls < 0:
+        raise ValueError("balls must be a non-negative integer.")
+    if not isinstance(box_sizes,list):
+        raise ValueError("box_sizes must be a non-empty list.")
+    capacity= 0
+    for size in box_sizes:
+        if not isinstance(size, int):
+            raise TypeError("box_sizes must contain only positive integers.")
+        if size < 1:
+            raise ValueError("box_sizes must contain only positive integers.")
+        capacity+= size
+    if capacity < balls:
+        raise ValueError("The total capacity of the boxes is less than the "
+                         "number of balls to be distributed.")
+    box_sizes= list( sorted(box_sizes)[::-1] )
+    return unlabeled_balls_in_unlabeled_boxe(balls, box_sizes)
+
+
+def unlabeled_balls_in_unlabeled_boxe(balls, box_sizes):
+    '''
+    @author Dr. Phillip M. Feldman
+    '''
+    if not balls:
+        yield len(box_sizes) * (0,)
+    elif len(box_sizes) == 1:
+        if box_sizes[0] >= balls:
+            yield (balls,)
+    else:
+        for balls_in_first_box in range( min(balls, box_sizes[0]), -1, -1 ):
+            balls_in_other_boxes = balls - balls_in_first_box
+            short = unlabeled_balls_in_unlabeled_boxe
+            for distribution_other in short(balls_in_other_boxes,
+                                            box_sizes[1:]):
+                if distribution_other[0] <= balls_in_first_box:
+                    yield (balls_in_first_box,) + distribution_other
 
 from helper import make_claw, make_diamond
 class Test(unittest.TestCase):
@@ -87,8 +190,12 @@ class Test(unittest.TestCase):
 
 
     def testValidColoring(self):
-        nodes = [0,1,2]
         g = make_claw()
+        # test invalid claw coloring
+        coloring = [[0, 1, 2, 3]]
+        valid = valid_coloring(coloring, g)
+        self.assertEqual(valid, False,
+                         "Valid coloring: Failed for one coloring on claw")
         # test valid claw coloring
         coloring = [[0], [1, 2, 3]]
         valid = valid_coloring(coloring, g)
@@ -108,22 +215,41 @@ class Test(unittest.TestCase):
         coloring = [[3], [2], [0, 1]]
         valid = valid_coloring(coloring, g)
         self.assertEqual(valid, False, 
-                         "Valid coloring: failed for invalid coloring on diamond")
+                         '''
+                         Valid coloring: failed for invalid coloring on diamond
+                         ''')
 
-    def testColorPermutations(self):
-        # very simple tests
-        nodes = [1, 1, 1]
-        baskets = 2
-        expect = [
-                    [[1, 1], [1]],
-                    [[1, 1, 1], []]
-                 ]
-        for color in color_permutations(nodes, baskets):
-            print(color)
-            self.assertEqual(color in expect, True, 
-                             '''Color Permuations Simple Test: %s extra case''' 
-                             % color)
+    def testChromaticNumber(self):
+        g = make_claw()
+        chromatic = chromatic_number(g)
+        expect = 2
+        self.assertEqual(expect, chromatic, "Chromatic Number: Claw Case")
+        g = make_diamond()
+        chromatic = chromatic_number(g)
+        expect = 3
+        self.assertEqual(expect, chromatic, "Chromatic Number: Diamond Case")
 
+
+    def testValidSplit(self):
+        split = (4, 0)
+        self.assertEqual(valid_split(split), False,
+                         "Valid split: True on Invalid Split")
+        split = (4, 1)
+        self.assertEqual(valid_split(split), True,
+                         "Valid split: True on Valid Split")
+
+    def testConvertCombo(self):
+        combo = (4, 1)
+        conversion = convert_combo(combo)
+        self.assertEqual(type(conversion), list,
+                         "Convert Combo: did not return list")
+
+    def testAssembleColoring(self):
+        split = [1, 2]
+        combo = [1, 2, 3]
+        result = assemble_coloring(combo, split)
+        expect = [[3], [2, 1]]
+        self.assertEqual(expect, result, "Assemble Coloring: unexpected result")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
