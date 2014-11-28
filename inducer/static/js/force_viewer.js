16e6421bf5405f36e80cc95784c07ad517d861c8
fras2560 <fras2560@mylaurier.ca>
2014-11-28 13:58:15 -0500

    Added the Co-A graph

--- inducer/static/js/force_viewer.js
+++ inducer/static/js/force_viewer.js
@@ -143,6 +143,7 @@ var GRAPHS ={
                   co_twin:{nodes:six_nodes, edges:[[0, 1], [0, 2], [0,5], [1, 2], [1, 5], [2, 3], [3, 4], [4, 5]]},
                   co_twin_house:{nodes:six_nodes, edges:[[0, 1], [1, 2], [1, 3], [2, 3], [2, 4], [3, 4], [4, 5]]},
                   W5:{nodes:six_nodes, edges:[[0, 1], [0, 4], [1, 2], [2, 3], [3, 4], [0, 5], [1, 5], [2, 5], [3, 5], [4, 5]]},
+                  co_A:{nodes:six_nodes, edges:[[0, 1], [0, 2], [1, 2],[1, 3], [2, 3], [2, 4], [3, 4], [3, 5], [4, 5]]}
                 };
 // add keyboard callback
 d3.select(window)
