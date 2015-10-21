6ea059400dcfc47b6bb4400dd1c0edb9da1c7bbd
fras2560 <fras2560@mylaurier.ca>
2015-10-21 13:55:47 -0400

    Add another test case

--- inducer/critical.py
+++ inducer/critical.py
@@ -44,5 +44,7 @@ from inducer.helper import make_claw, make_wheel
 class Tester(unittest.TestCase):
     def testCritical(self):
         self.assertEqual(critical(make_claw()), None)
+        self.assertEqual(critical(make_claw(), dense=True), None)
         self.assertEqual(critical(make_wheel(6)), 4)
+        self.assertEqual(critical(make_wheel(6), dense=True), 4)
         self.assertEqual(critical(make_wheel(7)), None)
